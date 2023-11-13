b6a52201a0a58503d59c4406181a31a3fe186062
danil <serdaniel596@gmail.com>
2023-11-13 22:28:46 +0900

    Add RegisterAndLogin and UserDTO classes

---
+++ src/main/java/com/example/demo/entity/UserDTO.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "UserDTO")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "link")
+    private String link;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+}
